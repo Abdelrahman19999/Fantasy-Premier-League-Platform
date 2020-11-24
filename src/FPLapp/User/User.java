a2578f481a57c424f9d2c3addc019ed60d69724d
Bassel Yasser <basselyasser42@gmail.com>
2020-11-24 19:15:16 +0200

    User setters n getters

--- src/FPLapp/User/User.java
+++ src/FPLapp/User/User.java
@@ -3,8 +3,47 @@ package FPLapp.User;
 public class User {
 	private String Name;
 	private String Email;
-	private String password;
+	private String Password;
 	private int ID;
 	
+	public void setName(String Name)
+	{
+		this.Name = Name;
+	}
+	
+	public void setEmail(String Email)
+	{
+		this.Email = Email;
+	}
+	
+	public void setPassword(String Password)
+	{
+		this.Password = Password;
+	}
+	
+	public void setID(int ID)
+	{
+		this.ID = ID;
+	}
+	
+	public String getName()
+	{
+		return Name;
+	}
+	
+	public String getEmail()
+	{
+		return Email;
+	}
+	
+	public Boolean comparePassword(String enteredPassword)
+	{
+		return Password.equals(enteredPassword);
+	}
+	
+	public int getID()
+	{
+		return ID;
+	}
 	
 }
--- src/FPLapp/User/UserDao.java
+++ src/FPLapp/User/UserDao.java
@@ -2,7 +2,7 @@ package FPLapp.User;
 
 public interface UserDao {
      public ArrayList<User> getAllUsers();
-     public void updateUser(User);
-     public void addUser(User);
-     public boolean deleteUser(User);
+     public void updateUser(User user);
+     public void addUser(User user);
+     public boolean deleteUser(User user);
 }
