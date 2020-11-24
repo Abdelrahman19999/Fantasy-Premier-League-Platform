8a954cd73aebf75eb5693a2ad368f2fd9f2d7a63
Bassel Yasser <basselyasser42@gmail.com>
2020-11-25 00:05:42 +0200

    Update Authenticator.java

--- src/FPLapp/Authenticator.java
+++ src/FPLapp/Authenticator.java
@@ -15,23 +15,21 @@ public class Authenticator {
     }
     
     public boolean checkAuthority(Scanner scanner) {
-        //Scanner scanner = new Scanner(System.in);
+ 
         System.out.print("Email: ");
         String email = scanner.nextLine();
         System.out.print("Password: ");
         String password = scanner.nextLine();
         for(User user : userdata.getAllUsers()) {
             if(user.getEmail().compareTo(email) == 0) {
-            	//scanner.close();
                 return user.getPassword().equals(password);
             }
         }
-       // scanner.close();
+      
         return false;
     }
     
     public boolean checkAvailabilty(Scanner scanner){
-        //Scanner scanner = new Scanner(System.in);
         System.out.print("Name: ");
         String newname = scanner.nextLine();
         System.out.print("Email: ");
@@ -41,12 +39,10 @@ public class Authenticator {
         int newID = userdata.getAllUsers().size();
         for (User user : userdata.getAllUsers()){
             if(user.getEmail().compareTo(newemail) == 0) {
-            	//scanner.close();
                 return false;
             }
         }
         userdata.addUser(newname, newemail, newpassword, newID);
-        //scanner.close();
         return true;
     }
 }
