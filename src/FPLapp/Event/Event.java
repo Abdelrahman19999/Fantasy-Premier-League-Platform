3b93717a066e1c77396b02c55a20eb1aaf2a6265
Bassel Yasser <basselyasser42@gmail.com>
2021-01-04 22:08:24 +0200

    Update Event.java

--- src/FPLapp/Event/Event.java
+++ src/FPLapp/Event/Event.java
@@ -1,5 +1,30 @@
 package FPLapp.Event;
 
-public class Event {
+import java.util.ArrayList;
 
+public class Event {
+	int gameWeek;
+	private ArrayList<EventObserver> observers;
+	
+	public void registerObserver(EventObserver eventOb)
+	{
+		observers.add(eventOb);
+	}
+	
+	public void removeObserver(EventObserver eventOb)
+	{
+		observers.remove(observers.indexOf(eventOb));
+	}
+	
+	public void notifyObserver()
+	{
+		for(EventObserver ob : observers)
+		{
+			ob.update();
+		}
+	}
+	
+	public void getPlayerInfo() {};
+	public void getMatchInfo() {};
+	
 }
